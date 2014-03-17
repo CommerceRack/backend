f7ff6afa9858af780d178167129602c38d4076df
Brian Horakh <brianh@zoovy.com>
2014-03-17 22:47:33 +0000

    added amazon ses support to BLAST::RECIPIENT::EMAIL

--- lib/BLAST/RECIPIENT.pm
+++ lib/BLAST/RECIPIENT.pm
@@ -3,8 +3,8 @@ package BLAST::RECIPIENT;
 use strict;
 
 sub bcc { return(undef); }
-sub blaster { return(@_[0]->{'*BLASTER'}); }
-sub username { return(@_[0]->{'*BLASTER'}->username()); }
-sub meta { return(@_[0]->{'%META'} || {}); }
+sub blaster { return($_[0]->{'*BLASTER'}); }
+sub username { return($_[0]->{'*BLASTER'}->username()); }
+sub meta { return($_[0]->{'%META'} || {}); }
 
 1;
--- lib/BLAST/RECIPIENT/EMAIL.pm
+++ lib/BLAST/RECIPIENT/EMAIL.pm
@@ -5,7 +5,7 @@ use parent 'BLAST::RECIPIENT';
 
 require MIME::Lite;
 
-sub email { return(@_[0]->{'EMAIL'}); }
+sub email { return($_[0]->{'EMAIL'}); }
 
 sub new {
 	my ($class, $BLASTER, $EMAIL, $METAREF) = @_;
@@ -30,7 +30,67 @@ sub send {
 
 	my $SUBJECT = $msg->subject();
 	$SUBJECT =~ s/<.*?>//gs;	# html stripping!
+
+
+	my %EMAIL = ();
+	if ((defined $webdbref->{'%plugin.esp_awsses'}) && ($webdbref->{'%plugin.esp_awsses'}->{'enable'})) {
+		##
+      ##                              'iam-username' => '20131126-200727',
+      ##                              'enable' => '0',
+      ##                              'ts' => '1385525408',
+      ##                             'from_email_campaign' => '',
+      ##                              'from_email_support' => '',
+      ##                              '~smtp-password' => 'AoilvAuYpxeDUd60lS96x2yrvY5hf1zCKBK3ahAgBJxd',
+      ##                              'luser' => 'support/kimh',
+      ##                              'from_email_auto' => '',
+      ##                              'smtp-username' => 'AKIAI5RZAP3S2BYEDCIA'
+		##
+		%EMAIL = %{$webdbref->{'%plugin.esp_awsses'}};
+		foreach my $k (keys %EMAIL) {
+			if (substr($k,0,1) eq '~') {
+				$EMAIL{substr($k,1)} = $EMAIL{$k}; 	## change ~smtp-password into smtp-password
+				}
+			}
+		$EMAIL{'esp'} = 'awsses';
+
+		my $ses = Net::AWS::SES->new(
+			access_key => $EMAIL{'smtp-username'}, 
+			secret_key => $EMAIL{'smtp-password'},
+			);
+
+		my $r = undef;
+		eval { $r = $ses->send($msg); };
+		}
+	else {
+		##
+		%EMAIL = (
+			'esp'=>'postfix',
+			'from_email_campaign'=>''
+			);
+		my $msg = MIME::Lite->new(
+			'X-Mailer'=>"CommerceRack $JSONAPI::VERSION",
+			'Reply-To'=>$FROM,
+			'Errors-To'=>$FROM,
+			'Return-Path'=>$FROM,
+			'Disposition'=>'inline',
+			From => $FROM,
+			To => $RECIPIENT,
+			Bcc => $BCC,
+			Subject => $SUBJECT,
+			Type=>'text/html',
+			Data => $BODY,
+#			  Encoding => 'quoted-printable'
+			);
+
+		$msg->attr("content-type"			=> "text/html");
+		$msg->attr("content-type.charset" => "US-ASCII");
+#		  $msg->attr("content-type.name"	 => "homepage.html");
 	
+		my $qtFROM = quotemeta($FROM);
+		$msg->send("sendmail", "/usr/lib/sendmail -t -oi -B 8BITMIME -f $FROM");
+		# MIME::Lite->send("sendmail", "/bin/cat >/tmp/foo");
+		}
+
 #	my $BODY = $result->{'BODY'};
 #		my %v = ();
 #		if ($ENV{'REMOTE_ADDR'}) { $v{'ip'} = $ENV{'REMOTE_ADDR'}; }
@@ -45,28 +105,6 @@ sub send {
 #		chop($str);
 #		# print "STR: $str\n";
 
-	my $msg = MIME::Lite->new(
-		'X-Mailer'=>"CommerceRack $JSONAPI::VERSION",
-		'Reply-To'=>$FROM,
-		'Errors-To'=>$FROM,
-		'Return-Path'=>$FROM,
-		'Disposition'=>'inline',
-		From => $FROM,
-		To => $RECIPIENT,
-		Bcc => $BCC,
-		Subject => $SUBJECT,
-		Type=>'text/html',
-		Data => $BODY,
-#		  Encoding => 'quoted-printable'
-		);
-
-	$msg->attr("content-type"			=> "text/html");
-	$msg->attr("content-type.charset" => "US-ASCII");
-#	  $msg->attr("content-type.name"	 => "homepage.html");
-
-	my $qtFROM = quotemeta($FROM);
-	$msg->send("sendmail", "/usr/lib/sendmail -t -oi -B 8BITMIME -f $FROM");
-	# MIME::Lite->send("sendmail", "/bin/cat >/tmp/foo");
 	return(1);
 	}
 
