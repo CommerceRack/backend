83fa24b8ef6adbad59d8908f5e4f5c64616304ae
Brian Horakh <brianh@zoovy.com>
2014-06-25 23:03:04 +0000

    install phantomjs with 201406

---
+++ patches/201406/03-phantomjs.sh
@@ -0,0 +1,12 @@
+
+if [ ! -f phantomjs-1.9.7-linux-x86_64.tar.bz2 ] ; then
+	cd /usr/local
+	wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
+	tar -xjvf phantomjs-1.9.7-linux-x86_64.tar.bz2*
+	cd phantomjs-1.9.7-linux-x86_64
+	mv /usr/phantomjs-1.9.7-linux-x86_64 /usr/local/phantomjs
+	cd /usr/local/bin
+	ln -s /usr/local/phantomjs/bin/phantomjs phantomjs
+fi;
+
+                          
