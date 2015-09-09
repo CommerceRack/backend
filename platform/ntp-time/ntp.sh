a1ff19de5f58895fdd7ebbe1f0e49802b5870c10
root <root@naboo.(none)>
2015-09-09 13:19:25 -0700

    adding chkout_phone to the config.js file

--- platform/ntp-time/ntp.sh
+++ platform/ntp-time/ntp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 exec >/dev/null 2>&1
-if /usr/sbin/ntpdate -u 208.74.184.18; then
+if /usr/sbin/ntpdate -u utcnist.colorado.edu ; then
   exit 0
 else
-  if /usr/sbin/ntpdate -u 208.74.184.19; then
+  if /usr/sbin/ntpdate -u utcnist2.colorado.edu ; then
     exit 0
   else
     logger -p cron.err "Cannot fetch network time."
